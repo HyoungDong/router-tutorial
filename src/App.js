8251b913eff1666aed16d9792a08016c8dff75ff
HyoungDong <52812307+HyoungDong@users.noreply.github.com>
2021-08-04 11:52:12 +0900

    history & withRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
+import HistorySample from './HistorySample';
 import Home from './Home';
-import Profile from './Profile';
 import Profiles from './Profiles';
+import WithRouterSample from './WithRouterSample';
 
 function App() {
   return (
@@ -18,11 +19,16 @@ function App() {
         <li>
           <Link to="/profiles">프로필</Link>
         </li>
+        <li>
+          <Link to="/history">예제</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} exact />
       <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
+      <WithRouterSample />
     </div>
   );
 }
